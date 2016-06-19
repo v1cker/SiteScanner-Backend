f9f8c9f23dd1858a2e0089ecfa82e2ffd528e9e6
Josowsky <bartosz@jozwowiak.pl>
2016-06-19 22:32:24 +0200

    App "sites_backup" added

---
+++ sites_backup/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import SitesBackupIndex
+
+
+class SitesBackupIndexAdmin(admin.ModelAdmin):
+    list_display = ['id', 'entry', 'path', 'author', 'timestamp']
+
+
+admin.site.register(SitesBackupIndex, SitesBackupIndexAdmin)
---
+++ sites_backup/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from register_site.models import EntriesIndex
+
+
+class SitesBackupIndex(models.Model):
+    entry = models.ForeignKey(EntriesIndex)
+    path = models.CharField(max_length=255, null=False)
+    author = models.CharField(max_length=120, default='Anonymous')
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return str(self.id)
